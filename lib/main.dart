7e1563b39a0fde6eebcc37641dc116119bbe981c
Steven Mai <stevenmai826@gmail.com>
2021-02-10 20:41:06 -0800

    FIxed RangeError bug

--- lib/main.dart
+++ lib/main.dart
@@ -78,7 +78,7 @@ class Painter extends CustomPainter {
       ..isAntiAlias = true
       ..strokeWidth = 3.0;
 
-    for (var i = 0; i < offsets.length; i++) {
+    for (var i = 0; i < offsets.length - 1; i++) {
       if (offsets[i] != null && offsets[i + 1] != null) {
         canvas.drawLine(offsets[i], offsets[i + 1], paint);
       } else if (offsets[i] != null && offsets[i + 1] == null) {
