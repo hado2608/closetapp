d63c2cc77dd86140e23e74d030f18cad787df64b
Mikayla Brunner <73600726+mikayla-rb@users.noreply.github.com>
2021-03-08 14:25:10 -0600

    category screen in progess

--- lib/screens/categoryscreen.dart
+++ lib/screens/categoryscreen.dart
@@ -6,6 +6,7 @@ import 'package:closetapp/clothingitem.dart';
 import 'dart:math';
 import 'homepage.dart';
 import 'package:closetapp/helpers.dart';
+import 'details_page.dart';
 
 class CategoryScreen extends StatefulWidget {
   @override
@@ -14,78 +15,14 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
-  final File image;
-  final String name;
-  final String category;
-  final String id;
   final _formKey = GlobalKey<FormState>();
   ClothingDatabase clothingDatabase;
+  ClothingItem clothingItem;
   UI.Image img;
   bool isImageLoaded = false;
-  String clothingName;
-  String categoryName;
-  String itemPath;
-  _CategoryScreenState(
-      {@required this.image,
-      @required this.name,
-      @required this.category,
-      @required this.id});
 
-//   int _value = 1;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text('Edit'),
-//         ),
-//         body: Padding(
-//             padding: const EdgeInsets.all(16.0),
-//             child: Column(
-//               children: <Widget>[
-//                 TextField(
-//                     decoration: InputDecoration(
-//                         labelText: 'Title',
-//                         hintText: 'Give the clothes a name')),
-//                 new DropdownButton(
-//                     value: _value,
-//                     items: [
-//                       DropdownMenuItem(
-//                         child: Text("Shirts"),
-//                         value: 1,
-//                       ),
-//                       DropdownMenuItem(
-//                         child: Text("Pants"),
-//                         value: 2,
-//                       ),
-//                       DropdownMenuItem(child: Text("Accessories"), value: 3),
-//                       DropdownMenuItem(child: Text("Other"), value: 4)
-//                     ],
-//                     onChanged: (value) {
-//                       setState(() {
-//                         _value = value;
-//                       });
-//                     }),
-//                 SizedBox(
-//                   height: 20,
-//                 ),
-//                 RaisedButton(
-//                   onPressed: () {},
-//                   color: Colors.blue,
-//                   child: Text(
-//                     'Save',
-//                     style: TextStyle(color: Colors.white),
-//                   ),
-//                 ),
-//               ],
-//             )));
-//   }
-// }
-  void createImageFile() async {
-    itemPath = generateImageName();
-    final imageFile = await pathForImage(itemPath);
-    final bytes = await img.toByteData(format: UI.ImageByteFormat.png);
-    await imageFile.writeAsBytes(bytes.buffer.asUint8List());
-  }
+  _CategoryScreenState(
+      {@required this.clothingItem, @required this.clothingDatabase});
 
   @override
   Widget build(BuildContext context) {
@@ -98,64 +35,56 @@ class _CategoryScreenState extends State<CategoryScreen> {
               key: _formKey,
               child: Column(children: <Widget>[
                 TextFormField(
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please name your clothing item.';
-                    } else {
-                      clothingName = value;
-                      return null;
-                    }
-                  },
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please categorize your clothing item.';
-                    } else {
-                      categoryName = value;
-                      return null;
-                    }
-                  },
-                  decoration: InputDecoration(labelText: 'Category Name'),
-                ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please name your clothing item.';
+                      } else {
+                        clothingItem.name = value;
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Clothing Name',
+                    )),
+                DropdownButton(
+                    value: clothingItem.category,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text('Shirt'),
+                        value: 'Shirt',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('Bottoms'),
+                        value: 'Bottoms',
+                      ),
+                      DropdownMenuItem(child: Text('Shoes'), value: 'Shoes')
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        clothingItem.name = value;
+                      });
+                    }),
               ])),
-          CustomPaint(painter: DisplayImage(img), child: Container()),
           ElevatedButton(
             onPressed: () {
-              if (_formKey.currentState.validate()) {
-                createImageFile();
-                clothingDatabase.insert(new ClothingItem(
-                    id: "$clothingName" +
-                        (new Random().nextInt(1000)).toString(),
-                    name: clothingName,
-                    category: categoryName,
-                    imagePath: itemPath));
-                // Scaffold.of(context)
-                //     .showSnackBar(SnackBar(content: Text('Processing Data')));
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                );
-              } else {
-                return null;
-              }
+              print("!!!!!!" + clothingItem.name);
+              // clothingDatabase.updateClothingItem(clothingItem);
+              // if (_formKey.currentState.validate()) {
+              //   createImageFile();
+              //   clothingDatabase.insert(new ClothingItem(
+              //       id: "$clothingName" +
+              //           (new Random().nextInt(1000)).toString(),
+              //       name: clothingName,
+              //       category: categoryName,
+              //       imagePath: itemPath));
+              // Scaffold.of(context)
+              //     .showSnackBar(SnackBar(content: Text('Processing Data')));
+              Navigator.pop(context);
+              // } else {
+              //   return null;
             },
             child: Text('Save'),
           )
         ]));
   }
-}
-
-class DisplayImage extends CustomPainter {
-  UI.Image im;
-
-  DisplayImage(this.im);
-  @override
-  void paint(UI.Canvas canvas, UI.Size size) {
-    canvas.drawColor(Colors.white, BlendMode.src);
-    canvas.drawImage(im, new Offset(0, 0), new Paint());
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
